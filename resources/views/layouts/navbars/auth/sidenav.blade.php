a170e9422d387c72599e890ecbdb2f80d79bbcd6
Jhon <jhonxcd@gmail.com>
2023-05-24 15:41:59 -0500

    Redireccionando a vista productos despues de logearse

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -3,8 +3,7 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="{{ route('home') }}"
-            target="_blank">
+        <a class="navbar-brand m-0" href="{{ route('clientes.index') }}">
             <img src="./img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold">ADMINISTRACION</span>
         </a>
@@ -52,7 +51,7 @@
                     <span class="nav-link-text ms-1">Salida Productos</span>
                 </a>
             </li>
-          
+
             <li class="nav-item">
                 <a class="nav-link " href="{{ route('categoria.index') }}">
                     <div
